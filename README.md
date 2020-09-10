8cace88bc29fda74227a8b0e21da6609d657ce97
Florian VERNERET <florian.verneret@sadem-group.fr>
2020-09-10 10:41:13 +0200

    [ADD] Package

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-geocoder-french
+# geocoder-french
+
+Geocoder & reverse-geocoder for French territory. **(Fork from [geocoder-fr](https://www.npmjs.com/package/geocoder-fr))**  
+>This version correct the original geocoder-fr by adding a true latitude & longitude result on geocode() method.  
+>To avoid a break on the original behavior of **[geocoder-fr](https://www.npmjs.com/package/geocoder-fr)**, i decide to create this fork  
+  
+
+* No API key is required, this package use French gouv opendata API.
+* Requests results are geojson formated.
+* All package function are async.
+* This package can be used with node or react 
+
+## Usage
+
+```
+var geocoder = require('geocoder-french')
+```
+
+## Examples
+
+### Get all location matching with address
+
+```
+geocoder.geocode('47 rue de l\'arquebuse 08000 charleville').then((addr)=> console.log(addr));
+
+```
+
+### Get all address matching with GPS lat & long (WGS84 coords)
+
+```
+geocoder.reverse(4.724433, 49.770714).then((coords)=> console.log(coords));
+```
+
+### Get most accurate location matching with address
+
+```
+geocoder.geocodeBestResult('47 rue de l\'arquebuse 08000 charleville').then((addr)=> console.log(addr));
+
+```
+
+### Get most accurate address matching with GPS lat & long (WGS84 coords)
+
+```
+geocoder.reverseBestResult(4.724433, 49.770714).then((coords)=> console.log(coords));
+```
+
+## Test
+
+```
+node test/test.js
+```
+___
+>Thanks to Julien G for his great job on **[geocoder-fr](https://www.npmjs.com/package/geocoder-fr)**
---
+++ src/geocoder-french.js
@@ -0,0 +1,68 @@
+const fetch = require('node-fetch');
+
+
+class GeocoderFrench {
+    /**
+     *  Geocode a string
+     *
+     *  @param  addr        string to be geocoded
+     *  @return Promise
+     */
+    static async geocode(addr) {
+        const response  = await fetch('https://api-adresse.data.gouv.fr/search/?q='+addr);
+
+        return await response.json();
+    }
+
+    /**
+     *  Reverse geocode a lat long
+     *
+     *  @param  long    longitude
+     *  @params lat     latitude
+     *  @return Promise
+     */
+    static async reverse(long, lat) {
+        const response  = await fetch('https://api-adresse.data.gouv.fr/reverse/?long='+long+'&lat='+lat);
+
+        return await response.json();
+    }
+
+    /**
+     *  Geocode a string and return most accurate result
+     *
+     *  @param  addr        string to be geocoded
+     *  @return Promise
+     */
+    static async geocodeBestResult(addr) {
+        const data = await this.geocode(addr);
+
+        if(data.features.length === 0)
+            return null;
+
+        return {
+            x: data.features[0].properties.x,
+            y: data.features[0].properties.y,
+            long: data.features[0].geometry.coordinates[0],
+            lat: data.features[0].geometry.coordinates[1]
+        };
+    }
+
+    /**
+     *  Reverse geocode a lat long and return most accruate result
+     *
+     *  @param  long    longitude
+     *  @param  lat     latitude
+     *  @return Promise
+     */
+    static async reverseBestResult(long, lat) {
+        const data = await this.reverse(long, lat);
+
+        if(data.features.length === 0)
+            return null;
+
+        return data.features[0].properties;
+    }
+}
+
+
+module.exports = GeocoderFrench;
